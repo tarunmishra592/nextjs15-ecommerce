b94438ca7115f8abbdb3fc21f9c7cac210d842b8
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-06 18:56:35 +0530

    Initial commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ const app = express();
 
 // Middlewares
 const allowedOrigins = [
-    'https://nextjs15-ecommerce-sooty.vercel.app', // Vercel frontend
+    'https://nextjs15-ecommerce-rvcc.vercel.app', // Vercel frontend
     'http://localhost:3000', // local dev
     'http://localhost:3001' // local dev
   ];
