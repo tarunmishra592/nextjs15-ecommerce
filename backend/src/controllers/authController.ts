20c073123050963619ebc7f9faecb14a57316dad
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-06 14:19:16 +0530

    Initial Commit

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -15,6 +15,13 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   try {
     const { email, password } = req.body;
     const data = await authService.login(email, password);
+    res.cookie('token', data.token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: 24 * 60 * 60 * 1000, // e.g., 7 days
+      path: '/',
+    });
     res.status(200).json({ token: data.token, user: data.user});
   } catch (err) {
     next(err);
