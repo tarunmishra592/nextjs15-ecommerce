233e5c346c58658fe8c6967c8ee89975708b3eb4
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-06 18:49:26 +0530

    Initial commit

--- frontend/src/app/products/[id]/page.tsx
+++ frontend/src/app/products/[id]/page.tsx
@@ -13,6 +13,7 @@ export default async function ProductPage({ params }: Props) {
   const product: any = await fetch(`https://nextjs15-ecommerce-rvcc.vercel.app/api/products/${id}`);
   const productReviews: any = await fetch(`https://nextjs15-ecommerce-rvcc.vercel.app/api/products/${id}/reviews`);
 
+  console.log('product', product)
 
   return (
     <div className="max-w-6xl mx-auto p-4 space-y-8">
