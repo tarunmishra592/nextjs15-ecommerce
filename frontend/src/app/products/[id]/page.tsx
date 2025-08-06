711074540600647443bd9c5605520c9bb19c1007
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-06 14:40:28 +0530

    Initial Commit

--- frontend/src/app/products/[id]/page.tsx
+++ frontend/src/app/products/[id]/page.tsx
@@ -1,20 +1,6 @@
-import ProductActions from '@/components/ProductActions/ProductActions';
-import ProductImageGallery from '@/components/ProductImageGallery/ProductImageGallery';
-import { apiFetch } from '@/lib/api';
-import { Product, Review } from '@/types';
-
-type Props = {
-  params: Promise<{ id: string }>
-}
 
 
-export default async function ProductPage({ params }: Props) {
-
-  const { id } = await params;
-
-  const product: Product = await apiFetch(`/products/${id}`);
-  const productReviews: Review[] = await apiFetch(`/products/${id}/reviews`);
-
+export default async function ProductPage() {
 
   return (
     <div className="max-w-6xl mx-auto p-4 space-y-8">
