64085a1fc25cada47bc5878d0f9d774b4d8255ea
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-06 13:26:05 +0530

    Initial Commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,9 @@
 // app/page.tsx
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-
 import HeroSlider from '@/components/HeroSlider/HeroSlider'
 import ProductCarousel from '@/components/ProductCarousel/ProductCarousel'
-import { apiFetch } from '@/lib/api'
-import { Product } from '@/types'
 
 export default async function HomePage() {
-
-  
-  const [featured, newArrivals] = await Promise.all([
-    apiFetch('/products?tags=featured&limit=8'),
-    apiFetch('/products?tags=new&limit=8'),
-  ]) as [Product[], Product[]];
 
   return (
     <div className="space-y-16 pb-8"> {/* Added bottom padding */}
@@ -22,12 +11,12 @@ export default async function HomePage() {
       
       <ProductCarousel 
         title="Featured Products" 
-        products={featured} 
+        tag={'featured'} 
       />
 
       <ProductCarousel 
         title="New Arrivals" 
-        products={newArrivals} 
+        tag={'new'} 
       />
     </div>
   )
--- frontend/src/components/ProductCarousel/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel/ProductCarousel.tsx
@@ -1,22 +1,36 @@
 // components/ProductCarousel/ProductCarousel.tsx
 'use client'
 
-import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper/modules'
 import ProductCard from '../ProductCard/ProductCard'
 import { Product } from '@/types'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import { apiFetch } from '@/lib/api'
 
 interface ProductCarouselProps {
   title: string
-  products: Product[]
+  tag: string
 }
 
-export default function ProductCarousel({ title, products }: ProductCarouselProps) {
+export default function ProductCarousel({ title, tag }: ProductCarouselProps) {
   const swiperRef = useRef<any>(null)
+  const [products, setProducts] = useState<Product[]>([])
+
+
+  useEffect(() => {
+    async function getProducts(){
+      apiFetch<Product[]>(`/products?tags=${tag}&limit=8`)
+        .then((data: Product[]) => {
+          setProducts(data);
+        })
+        .catch(console.error);
+    }
+    getProducts()
+  }, [tag])
 
   return (
     <section className="max-w-6xl mx-auto p-4">
@@ -56,7 +70,7 @@ export default function ProductCarousel({ title, products }: ProductCarouselProp
           1024: { slidesPerView: 4 }
         }}
       >
-        {products.map((product) => (
+        {products.length > 0 && products.map((product) => (
           <SwiperSlide key={product._id}>
             <ProductCard product={product} />
           </SwiperSlide>
