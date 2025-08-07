44b17e32460c75920fa8a58f78f8d80c93b6d242
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-07 17:18:20 +0530

    Initial commit

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -15,17 +15,6 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   try {
     const { email, password } = req.body;
     const data = await authService.login(email, password);
-    // Get the requesting origin dynamically
-    const requestOrigin = req.get('origin') || '';
-    const isProduction = process.env.NODE_ENV === 'production';
-    res.setHeader('x-vercel-set-bypass-cookie', 'samesitenone');
-    res.cookie('token', data.token, {
-      httpOnly: true,
-      secure: true,        // Must be false for HTTP
-      sameSite: 'none',      // Use 'lax' for local testing
-      path: '/',
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
     res.status(200).json({ token: data.token, user: data.user});
   } catch (err) {
     next(err);
