d1116b1ddedd4a176af865f8c83f263b3e9aae01
Tarun Kumar <tarun.mishra592@gmail.com>
2025-08-07 09:44:46 +0530

    Initial commit

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -15,12 +15,16 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   try {
     const { email, password } = req.body;
     const data = await authService.login(email, password);
+    const isVercel = process.env.VERCEL === '1';
+    const frontendDomain = isVercel ? process.env.FRONTEND_URL : 'localhost';
+
     res.cookie('token', data.token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      maxAge: 24 * 60 * 60 * 1000, // e.g., 7 days
+      secure: true, // Must be true on Vercel
+      sameSite: 'none', // Required for cross-site cookies
+      maxAge: 24 * 60 * 60 * 1000,
       path: '/',
+      domain: isVercel ? `.${frontendDomain}` : frontendDomain
     });
     res.status(200).json({ token: data.token, user: data.user});
   } catch (err) {
